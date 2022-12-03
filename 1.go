729c6815238d80a8307801849909b5d2a5c8aad5
Gambrivius <claponsie@gmail.com>
2022-12-03 11:26:08 -0500

    Added an o(n) implementation of two sum that uses a hash map

--- 1.go
+++ 1.go
@@ -1,8 +1,8 @@
 package goLeet
 
-func twoSum(nums []int, target int) []int {
+
+func twoSumNaive(nums []int, target int) []int {
 	// naive O(n^2) implementation beats 8.96% on runtime and 65.9% on memory
-	/*
 	for i := range nums {
 		for j := range nums {
 			if i!=j {
@@ -11,9 +11,12 @@ func twoSum(nums []int, target int) []int {
 				}
 			}
 		}
-	}*/
+	}
+	return nil
+}
 
-	// final slight optimization, beats 31.7% on runtime and 93.83% on memory
+func twoSumBetter(nums []int, target int) []int {
+	// slight optimization, beats 31.7% on runtime and 93.83% on memory
 	// avoids checking the same indices twice
 	l := len(nums)
 	for i := 0; i < l; i++ {
@@ -26,3 +29,16 @@ func twoSum(nums []int, target int) []int {
     return nil
 }
 
+func twoSum(nums []int, target int) []int {
+	// employ a hashmap for O(n) runtime
+	// beats 48.64% on run time and 54.12% on memory
+	m := map[int]int{}
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		if j, ok := m[target - nums[i]]; ok {
+			return []int{j, i}
+		}
+		m[nums[i]] = i
+	}
+    return nil
+}
